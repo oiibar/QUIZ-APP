6d75acaa149e5bddb60aaef94630d5767835b356
oiibar <95119839+oiibar@users.noreply.github.com>
2022-03-07 10:59:14 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+/*const startQuiz = document.getElementById('start_quiz');
+const quizContainer = document.getElementById('quiz_container');
+const answerChoice1 = document.getElementById('choice1');
+const answerChoice2 = document.getElementById('choice2');
+const answerChoice3 = document.getElementById('choice3');
+const checkChoicce = document.getElementById('check');
+
+
+startQuiz.addEventListener('click', function (e) {
+	e.preventDefault();
+	quizContainer.classList.toggle('active');
+});
+
+
+checkChoicce.addEventListener('click', function (e) {
+	e.preventDefault();
+	if (answerChoice1.checked) {
+		alert('correct');
+	} else if (!answerChoice1.checked && !answerChoice2.checked && !answerChoice3.checked) {
+		alert('answer')
+	} else {
+		alert('incorrect');
+	}
+});*/
